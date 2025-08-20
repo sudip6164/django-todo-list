c91c57690f5a76ba39a6a1ddc4e2b143d30f4997
sudip6164 <sudippradhanadgj@gmail.com>
2025-08-20 21:15:21 +0545

    task model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('L', 'Low'),
+        ('M', 'Medium'),
+        ('H', 'High'),
+    ]
+
+    title = models.CharField(max_length=255)
+    completed = models.BooleanField(default=False)
+    due_date = models.DateField(null=True, blank=True)
+    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES, default='M')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
