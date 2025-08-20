2c6883454b361245effc7a2accd16bb22c298bd1
sudip6164 <sudippradhanadgj@gmail.com>
2025-08-20 21:24:02 +0545

    task views

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
-# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        due_date = request.POST.get('due_date') or None
+        priority = request.POST.get('priority') or 'M'
+        if title:
+            Task.objects.create(title=title, due_date=due_date, priority=priority)
+        return redirect('index')
+
+    tasks = Task.objects.all().order_by('completed', 'due_date')
+    return render(request, 'todo/index.html', {'tasks': tasks})
+
+def delete_task(request, task_id):
+    Task.objects.filter(id=task_id).delete()
+    return redirect('index')
+
+def toggle_complete(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect('index')
